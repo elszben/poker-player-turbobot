b2995c46d6f723c5dbabb3ed0bf679715f2e6231
Laszlo Benedek <laszlo.benedek@ericsson.com>
2018-09-21 09:52:03 +0200

    Call

--- player.py
+++ player.py
@@ -6,7 +6,11 @@ class Player:
         print "Game state:", game_state
         current_buy_in = game_state["current_buy_in"]
         players = game_state["players"]
-        print current_buy_in, players
+        own_id = game_state["in_action"]
+        for player in players:
+            if player["id"] == own_id:
+                call_value = current_buy_in - player["bet"]
+                return call_value
         return 0
 
     def showdown(self, game_state):
