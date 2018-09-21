c815fe80f57ae71d7c3e259e75599f3bed3af99e
István Váradi <Istvan.Varadi@ericsson.com>
2018-09-21 11:08:57 +0200

    Rudimentary strategy further refined

--- strategy.py
+++ strategy.py
@@ -16,13 +16,13 @@ odds = [
 bet_chance = [
     [100, 0, 0, 0],
     [80, 20, 0, 0],
-    [30, 30, 40, 100],
-    [10, 40, 50, 100],
-    [5, 45, 50, 200],
-    [0, 50, 50, 200],
-    [0, 30, 70, 300],
-    [0, 20, 80, 1000],
-    [0, 0, 100, 1000]
+    [30, 30, 40, 10],
+    [10, 40, 50, 10],
+    [5, 45, 50, 20],
+    [0, 50, 50, 20],
+    [0, 30, 70, 30],
+    [0, 20, 80, 100],
+    [0, 0, 100, 100]
     ]
 
 card_value = {
@@ -43,7 +43,7 @@ card_value = {
 
 max_sum=69
 
-def get_bet(current_money, result, cards, rank):
+def get_bet(current_money, result, cards, rank, min_raise):
     card_sum = 0
     for card in cards:
         card_sum += card_value[card["rank"]]
@@ -59,21 +59,22 @@ def get_bet(current_money, result, cards, rank):
     elif r<=(chance[0] + chance[1]):
         return result if result<=max_raise else 0
     else:
-        return result + max_raise * r1 // 1000
+        ra = max_raise * r1 // 1000
+        return result + (ra if ra>=min_raise else 0)
 
 if __name__=="__main__":
     print get_bet(1000, 200, [{"rank":"8","suit":"hearts"},
                               {"rank":"8","suit":"spades"},
                               {"rank":"J","suit":"diamonds"},
                               {"rank":"J","suit":"clubs"},
-                              {"rank":"Q","suit":"diamonds"}], 2)
+                              {"rank":"Q","suit":"diamonds"}], 2, 30)
     print get_bet(1000, 200, [{"rank":"3","suit":"clubs"},
                               {"rank":"4","suit":"spades"},
                               {"rank":"7","suit":"spades"},
                               {"rank":"9","suit":"clubs"},
-                              {"rank":"Q","suit":"clubs"}], 0)
+                              {"rank":"Q","suit":"clubs"}], 0, 30)
     print get_bet(1000, 200, [{"rank":"6","suit":"diamonds"},
                               {"rank":"8","suit":"diamonds"},
                               {"rank":"10","suit":"diamonds"},
                               {"rank":"J","suit":"diamonds"},
-                              {"rank":"A","suit":"diamonds"}], 5)
+                              {"rank":"A","suit":"diamonds"}], 5, 30)
