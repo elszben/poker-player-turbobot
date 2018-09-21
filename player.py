64f6037e39e70d232d5f7976714af721c8fcebf4
Laszlo Benedek <laszlo.benedek@ericsson.com>
2018-09-21 09:44:22 +0200

    Log

--- player.py
+++ player.py
@@ -3,6 +3,10 @@ class Player:
     VERSION = "TurboBot 1"
 
     def betRequest(self, game_state):
+        print "Game state:", game_state
+        current_buy_in = game_state["current_buy_in"]
+        players = game_state["players"]
+        print current_buy_in, players
         return 0
 
     def showdown(self, game_state):
